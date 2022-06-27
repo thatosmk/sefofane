d28278fce412a6f6e231ec09dedeb2a33e284746
Thato Semoko <thatosmk@gmail.com>
2022-06-27 15:37:00 +0200

    adds a page to navigate to

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
 import logo from './images/logo.svg';
 import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App-header">
+      <nav className="navbar">
+        <ul className="flex items-center space-x-4">
+          <Link className="navbar-link" to="/">Home</Link>
+          <Link className="navbar-link" to="/pastLaunches">Past Launches</Link>
+        </ul>
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
